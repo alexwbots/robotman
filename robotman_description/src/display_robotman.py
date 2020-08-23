1165bf55a1362fec106fcb30245305e497ea121b
Alexander López <lopez.alexander@pucp.pe>
2020-08-22 23:16:23 -0500

    Update display_robotman.py

--- robotman_description/src/display_robotman.py
+++ robotman_description/src/display_robotman.py
@@ -8,9 +8,9 @@ from sensor_msgs.msg import Image
 if __name__ == '__main__':
     
   rospy.init_node("display_robotman")
-  img = cv2.imread('baxter_face.png')
+  img = cv2.imread('screen.png')
   msg = cv_bridge.CvBridge().cv2_to_imgmsg(img, encoding="bgr8")
-  pub = rospy.Publisher('/screen/eye_left/image', Image, latch=True, queue_size=1)
+  pub = rospy.Publisher('/screen/body/image', Image, latch=True, queue_size=1)
   rate = rospy.Rate(2)
 
   while not rospy.is_shutdown():
